1d6b2e634d60162769eb17cb4c7ed4a05a614e0e
Peter Rehm <peter.rehm@renvest.de>
2017-09-07 14:21:18 +0200

    Update to support Chrome 62+

--- src/DevToolsConnection.php
+++ src/DevToolsConnection.php
@@ -19,10 +19,10 @@ abstract class DevToolsConnection
         $this->url = $url;
     }
 
-    public function connect()
+    public function connect($url = null)
     {
         $options = ['fragment_size' => 2000000]; # Chrome closes the connection if a message is sent in fragments
-        $this->client = new Client($this->url, $options);
+        $this->client = new Client($url ?? $this->url, $options);
     }
 
     public function close()
