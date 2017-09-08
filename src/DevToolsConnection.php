1936fc64ca16a121b8fef990adb2c262084fe5de
Peter Rehm <peter.rehm@renvest.de>
2017-09-08 10:00:37 +0200

    Removed PHP7 code

--- src/DevToolsConnection.php
+++ src/DevToolsConnection.php
@@ -23,7 +23,7 @@ abstract class DevToolsConnection
     {
         $url = $url == null ? $this->url : $url;
         $options = ['fragment_size' => 2000000]; # Chrome closes the connection if a message is sent in fragments
-        $this->client = new Client($url ?? $this->url, $options);
+        $this->client = new Client($url, $options);
     }
 
     public function close()
